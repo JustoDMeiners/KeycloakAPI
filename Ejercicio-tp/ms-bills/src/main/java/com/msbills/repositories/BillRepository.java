685865f84cf51060cf6f948519603e699e518869
Justo Diaz Meiners <justito_dm@hotmail.com>
2023-08-23 01:04:05 -0300

    Comiteando

---
+++ Ejercicio-tp/ms-bills/src/main/java/com/msbills/repositories/BillRepository.java
@@ -0,0 +1,8 @@
+package com.msbills.repositories;
+
+import com.msbills.models.Bill;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BillRepository extends JpaRepository<Bill, String> {
+
+}
